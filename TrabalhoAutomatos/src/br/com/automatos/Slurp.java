def60ee930ee945ae93a0344822e52ba9c5c3fbf
dionatan.paris <dionatan.paris@gmail.com>
2015-09-29 11:49:26 -0300

    desenvolvimento do programa

--- TrabalhoAutomatos/src/br/com/automatos/Slurp.java
+++ TrabalhoAutomatos/src/br/com/automatos/Slurp.java
@@ -1,19 +1,44 @@
 package br.com.automatos;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class Slurp {
 
 	public static void main(String[] args) {
-		String slump = "DFDFDFDFG"; //DFG,EFG, DFFFFFG, DFDFDFDFG, DFEFFFFFG, EFFFFFFFEFG
-		String nslump = "DEEEG,";//EFAHG, DEFG, DG, EFFFFDG, DEEEG
-		String slimp = "ABAEFGCC";//, ABAHC, ABABAHCC, ADFGC, ADFFFGC, ABAEFGCC, ADFDFGC, ABABAEFGCCC"
-		String nslimp ="";
-		//String regex slump = "((DF|EF)(F*))*G{1}";
+		String regex_slurp  = "((AH)|[AB]+[H]C*|((AH)|(AB|A((DF|EF)(F*)*G{1})C*)))+(((DF|EF)(F*))*G{1})";
+		ArrayList<String> palavras = new ArrayList<>();
+		int nrlinha;
+		String arquivo = "C:\\Users\\dionatan.paris\\Desktop\\data.txt";	
 		
-		String regex = "(AH)|[AB]+[H]C*|((AH)|(AB|A((DF|EF)(F*)*G{1})C*))";
-		System.out.println(Pattern.matches(regex, slimp)); 
-
+		try {
+			FileReader arq = new FileReader(arquivo);
+			BufferedReader lerArq = new BufferedReader(arq);
+			nrlinha =  Integer.parseInt( lerArq.readLine());
+			
+			for (int i = 1; i <= nrlinha ;i++ ) {
+				palavras.add(lerArq.readLine());
+			}
+		arq.close();
+		
+		for (String palavra : palavras) {
+			if (Pattern.matches(regex_slurp, palavra)){
+				System.out.println("Sim");
+			}else{
+				System.out.println("Não");
+			}
+			
+		}
+		System.out.println("finalizado com sucesso!");
+		System.exit(0);
+		} catch (FileNotFoundException e) {
+			System.err.println("Arquivo não Encontrado!\n"+ e);
+		} catch (IOException e) {
+			System.err.println("Problema ao abrir o arquivo!\n"+e);
+		}
 	}
-
 }
